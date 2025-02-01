8504edf9d3683ad4965dde46378cb34e08029bea
bechirzarrouki <bechir.zarrouki00@gmail.com>
2025-02-01 23:52:41 +0100

    02

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -8,11 +8,25 @@ use Symfony\Component\Routing\Attribute\Route;
 
 final class HomeController extends AbstractController
 {
-    #[Route('/home', name: 'app_home')]
-    public function index(): Response
+    #[Route('/', name: 'app_home')]
+    public function home(): Response
     {
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
+        ]);
+    }
+    #[Route('/profile', name: 'app_profile')]
+    public function profile(): Response
+    {
+        return $this->render('profile/index.html.twig', [
+            'controller_name' => 'ProfileController',
+        ]);
+    }
+    #[Route('/search', name: 'app_search')]
+    public function search(): Response
+    {
+        return $this->render('search/index.html.twig', [
+            'controller_name' => 'SearchController',
         ]);
     }
 }
